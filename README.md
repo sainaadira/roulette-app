9a5f9290c18573fabd237060d29b027b909c4df5
Sai <snaadira@gmail.com>
2019-08-03 23:20:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Roulette Game Full-Stack Application
+Created a roulette game with casino owner authentication.
+
+CSS Credit:Raymond Ortega
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sai:casino@cluster0-2erzp.mongodb.net/gamble?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'gamble'
+};
